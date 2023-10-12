0ed696b0b4827c9f1468020bacb764da5994ce6d
RitaPylypaka <ritapylypaka@gmail.com>
2023-10-12 14:13:36 +0300

    Add project files.

---
+++ пр4.6/пр4.6.cpp
@@ -0,0 +1,73 @@
+﻿#include<iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    double P;
+    double S;
+    int k, i;
+    P = 1;
+    k = 1;
+    while (k <= 25)
+
+    {
+        S = 0;
+        i = 3;
+        while (i <= k)
+
+        {
+            S += sqrt(i * i + P * k) / i ;
+            i++;
+
+        }
+        P *= k;
+        k++;
+
+    }
+    cout << P << endl;
+    P = 1;
+    k = 1;
+    do
+    {
+        S = 0;
+        i = 3;
+        do
+        {
+            S += sqrt(i * i + P * k) / i;
+            i++;
+        } while (i <= k);
+        P *=  k;
+        k++;
+    } while (k <= 25);
+    cout << P << endl;
+    P = 1;
+    for (k = 1; k <= 25; k++)
+
+    {
+        S = 0;
+        for (i = 3; i <= k; i++)
+
+        {
+            S += sqrt(i * i + P * k) / i;
+
+        }
+        P *=  k;
+
+    }
+    cout << P << endl;
+    P = 1;
+    for (k = 25; k >= 1; k--)
+
+    {
+        S = 0;
+        for (i = k; i >= 1; i--)
+
+        {
+            S += sqrt(i * i + P * k) / i;
+
+        }
+        P *=  k;
+    }
+    cout << P << endl;
+    return 0;
+}
